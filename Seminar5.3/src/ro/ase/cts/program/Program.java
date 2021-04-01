4e58986adaa710c171a97dfdc24ea456c74b18dd
Georgiana <Georgiana@192.168.1.3>
2021-04-01 18:31:36 +0300

    Builder

--- Seminar5.3/src/ro/ase/cts/program/Program.java
+++ Seminar5.3/src/ro/ase/cts/program/Program.java
@@ -5,11 +5,11 @@ import ro.ase.cts.clase.Reteta;
 public class Program {
 	
 	public static void main(String[]args) {
+		
 		Reteta reteta1=new Reteta("Reteta1", 250);
 		Reteta reteta2=(Reteta) reteta1.copiaza();
 		
 		System.out.println(reteta1);
 		System.out.println(reteta2);
 	}
-
 }
